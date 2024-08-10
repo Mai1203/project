f3c46745770873b0fad125441fab69f2e89cb659
Mai1203 <83738249+Mai1203@users.noreply.github.com>
2024-08-09 19:47:07 -0500

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  
   return (
-    <div>Demo Api</div>
+    <div className='bg-slate-600 items-center'>Demo Api</div>
   )
 }
 
